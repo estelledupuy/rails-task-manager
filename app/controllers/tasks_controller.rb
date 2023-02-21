ba5f06ea0cfd9d218e1604bec14631950069a4eb
estelle dupuy <estelledupuy@yahoo.fr>
2023-02-21 13:54:15 +0100

    with some refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -19,17 +18,14 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to 'index_path', allow_other_host: true
   end
@@ -38,6 +34,10 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'index', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :new
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # get 'index', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
